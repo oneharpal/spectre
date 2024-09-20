16861ab101a79e376e5d0dc165ff833482fe2e4a
Harpal Singh Panwar <oneharpal@gmail.com>
2024-09-21 02:10:02 +0530

    Add project crud

---
+++ spec/views/projects/edit.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "projects/edit", type: :view do
+  let(:project) {
+    Project.create!(
+      title: "MyString",
+      description: "MyText"
+    )
+  }
+
+  before(:each) do
+    assign(:project, project)
+  end
+
+  it "renders the edit project form" do
+    render
+
+    assert_select "form[action=?][method=?]", project_path(project), "post" do
+
+      assert_select "input[name=?]", "project[title]"
+
+      assert_select "textarea[name=?]", "project[description]"
+    end
+  end
+end
