9683f746532748932e34c002f00e6f8ab311d8fe
Harpal Singh Panwar <oneharpal@gmail.com>
2024-10-08 23:43:33 +0530

    add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { build(:user) }
+
+  # Validation test
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without valid email' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without password' do
+    user.password = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid for duplicate email' do
+    create(:user, email: user.email)
+    expect(user).to_not be_valid
+    expect(user.errors[:email]).to include('has already been taken')
+  end
 end
