0574ba56611eec5cdc3075e782fdacd92ea83efd
Harpal Singh Panwar <oneharpal@gmail.com>
2024-10-14 18:55:10 +0530

    Add spec for project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:notes) }
+  end
+
+  describe 'immortal' do
+    it 'set true for new project if it is nil' do
+      project = Project.new(title: 'Test project title', description: 'Test description')
+      project.save
+      expect(project.immortal).to be(true)
+    end
+  end
 end
