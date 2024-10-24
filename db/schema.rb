cfda691d2f660ade45596ac407d2e629281f4aff
oneharpal <oneharpal@users.noreply.github.com>
2024-10-25 00:03:21 +0530

    Add paper_trail for record keeping (#11)
    * Add paper_trail for recod keeping
    
    * Update brakeman

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_13_165556) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_151110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_13_165556) do
     t.boolean "immortal"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.bigint "whodunnit"
+    t.datetime "created_at"
+    t.bigint "item_id", null: false
+    t.string "item_type", null: false
+    t.string "event", null: false
+    t.text "object"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "notes", "projects"
