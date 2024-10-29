cf45b5224deebd9d5187472c1ac471a230b45ca4
Harpal Singh Panwar <oneharpal@gmail.com>
2024-10-29 22:56:44 +0530

    Add message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_172300) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_172621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
